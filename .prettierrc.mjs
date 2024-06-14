40b062ea44b64e7f8956166419f89db568c06cb8
Carlos Saito <carlos@saito.style>
2024-06-14 11:32:03 +0200

    Add index.njk and prettier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,5 +1,13 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ["prettier-plugin-jinja-template"],
+  overrides: [
+    {
+      files: ["*.njk"],
+      options: {
+        parser: "jinja-template",
+      },
+    },
+  ],
 };
---
+++ apps/www.saito.style/README.md
@@ -0,0 +1,13 @@
+# My website
+
+https://saito.style (when its ready)
+
+## TODO
+
+1. Install fonts (Playfair Display, Work Sans)
+2. Create home page
+3. Create clock
+
+## Resumé page
+
+1. Systematize content somehow
